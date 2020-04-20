b8fd38af4504ef573b16e34d03a5aa1c3cd034de
Sebastiõ Dias Lutonda <lutonda@gmail.com>
2020-04-20 08:15:58 +0000

    fix bfa snach

--- snatch.php
+++ snatch.php
@@ -6,15 +6,15 @@ require __DIR__ . '/vendor/autoload.php'; // Composer's autoloader
 $date=date("Y-m-dTH:i:s");
 
 $sources = [
-    ['BFA', 'https://www.bfa.ao/pt/particulares/pesquisa-de-cambios/', 'table.table.table-style-1.js-exchangeTable'],
-    ['AGT', 'https://agt.minfin.gov.ao/PortalAGT/#!/', 'cambio table.table.table-condensed.table-blocked'],
-    ['Atlantico', 'https://www.atlantico.ao/pt/particulares/Pages/home.aspx', '.carousel.carousel-movefour.slide .carousel-inner'],
-    ['SOL', 'https://www.bancosol.ao/', 'table#CTRL_Cambios1_dynControl_quotationGrid'],
-    ['BNI', 'http://www.bni.ao/pt/taxas/', '.col-xs-12.rates-table table.table'],
-    ['BCI', 'https://www.bci.ao/', '.tabelas-container div.tabelas div.body'],
-    ['BPC', 'http://www.bpc.ao/bpc/pt/', '#foottoolbox .rightquarter .cambio tbody']
+    ['BFA', 'https://www.bfa.ao/pt/particulares/pesquisa-de-cambios/', 'table.table.table-style-1.js-exchangeTable','table.table.table-style-1.js-exchangeTable tbody tr:not(.highlight)'],
+    ['AGT', 'https://agt.minfin.gov.ao/PortalAGT/#!/', 'cambio table.table.table-condensed.table-blocked', 'cambio table.table.table-condensed.table-blocked'],
+    ['Atlantico', 'https://www.atlantico.ao/pt/particulares/Pages/home.aspx', '.carousel.carousel-movefour.slide .carousel-inner', '.carousel.carousel-movefour.slide .carousel-inner'],
+    ['SOL', 'https://www.bancosol.ao/', 'table#CTRL_Cambios1_dynControl_quotationGrid', 'table#CTRL_Cambios1_dynControl_quotationGrid'],
+    ['BNI', 'http://www.bni.ao/pt/taxas/', '.col-xs-12.rates-table table.table', '.col-xs-12.rates-table table.table'],
+    ['BCI', 'https://www.bci.ao/', '.tabelas-container div.tabelas div.body', '.tabelas-container div.tabelas div.body'],
+    ['BPC', 'http://www.bpc.ao/bpc/pt/', '#foottoolbox .rightquarter .cambio tbody', '#foottoolbox .rightquarter .cambio tbody']
 ];
-function getContent($x,$y,$z,$date){
+function getContent($w,$x,$y,$z,$date){
 
     $client = \Symfony\Component\Panther\Client::createChromeClient();
     // Or, if you care about the open web and prefer to use Firefox
@@ -26,10 +26,15 @@ function getContent($x,$y,$z,$date){
     //$client->clickLink('Support');
 
     // Wait for an element to be rendered
-    $crawler = $client->waitFor($z);
+    $crawler = $client->waitFor($w);
 
 
     $data=$crawler->filter($z)->text();
+
+    echo($data);
+    echo '---------------------------------------------';
+    echo '---------------------------------------------';
+    echo '---------------------------------------------';
     $url='?';
     $url.='source='.$x;
     $url.='&data=';
@@ -38,7 +43,7 @@ function getContent($x,$y,$z,$date){
     echo $url;
     //$client->takeScreenshot('screen.png'); // Yeah, screenshot!
 
-    submitData($url);
+   // submitData($url);
 
 
 }
@@ -61,5 +66,5 @@ function submitData($url)
 }
 
 foreach ($sources as $s) {
-    getContent($s[0],$s[1],$s[2],$date);
+    getContent($s[3],$s[0],$s[1],$s[2],$date);
 }
